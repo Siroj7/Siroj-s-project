0e8a532ac009b6c8a7583a9796144f58813a336b
Siroj <rustamovsiroj5@gmail.com>
2022-10-11 17:04:03 +0500

    my weekday project

---
+++ js/script.js
@@ -0,0 +1,457 @@
+/* 
+// let numberOfFilms = +prompt("Qancha film ko'rgansiz?");
+
+// let personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     test: 5,
+//     genres: [],
+//     privat: false
+// };
+
+// let lastFilm = prompt("Oxirgi ko'rgan filmlaringizdan biri?");
+// let review = +prompt("Uni qancha baholagan bo'lardingiz?");
+// for (let i = 0; i < 1; i++) {
+//     let lastFilm = prompt("Oxirgi ko'rgan filmlaringizdan biri?");
+//     let review = prompt("Uni qancha baholagan bo'lardingiz?");
+//     if (personalMovieDB.count < 10) {
+//         console.log("Ko'rilgan filmlar soni kam!");
+//     }
+//     if (personalMovieDB.count > 30) {
+//         console.log("Siz klassik tomoshabinsiz!");
+//     }
+//     if (personalMovieDB.count < 30) {
+//         console.log("Siz kino ishqibozisiz")
+//     }
+// }
+// personalMovieDB.movies[lastFilm] = review;
+
+// console.log(personalMovieDB);
+
+
+
+// function cutLetters(word, number) {
+//     if (word.length <= number) {
+//         console.log(word)
+//     } else if (word.length > number) {
+//         console.log(`${word.slice(0, number)}...`)
+//     }
+// }
+
+// cutLetters("Open Web", 5)
+
+// let num = prompt("plastik raqamni kiriting")
+// alert($ { num.substring(0, 4) } ** ** ** ** $ { num.substring(12, 16) }); 
+
+// let some = {
+//     name: 'Alex',
+//     surname: 'Smith',
+//     age: 20,
+//     test: "test",
+//     isMarried: true
+// }
+
+// for (let key in some) {
+//     if (key == 'age' || key == 'test') {
+//         continue;
+//     }
+
+//     console.log(some[key])
+// }
+
+
+// let a = prompt('Ismingizni kiritng...');
+// let b = prompt('Familiyangizni kiritng...');
+// let c = prompt('Yoshingizni kiritng...');
+
+
+// console.log(a, b, c)
+
+
+// let a = prompt('Ismingizni kiritng...');
+// let b = prompt('Familiyangizni kiritng...');
+// let c = prompt('Yoshingizni kiritng...');
+
+// let some = {
+//     name: a, 
+//     surname: b,
+//     age: c,
+// }
+
+// console.log(some)
+
+
+// let num2 = promp('son kiriting');
+
+
+
+// if (num2 < 50) {
+//     console.log('don')
+// }
+// if (num2 > 50) {
+//     console.log('error')
+// }
+
+
+
+// let c = prompt("Svetaforni ranglaridan birini kiriting:");
+
+// if (c == 'red') {
+//     console.log('yellow');
+// } else if (c == 'yellow') {
+//     console.log('green');
+// } else if (c == 'green') {
+//     console.log('red');
+// } else {
+//     console.log("Svetaforda bunaqa rang mavjud emas!")
+// }
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i + ". hello")
+// }
+
+
+
+
+
+
+
+
+
+// let numberOfFilms = prompt(`Qancha film ko'rgansiz?`);
+
+// let personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false,
+// }
+
+// let first = prompt(`Oxirgi ko'rgan filmlaringizdan biri?`);
+// let second = prompt(`Uni qancha baholagan bo'lardingiz?`);
+
+
+// personalMovieDB.movies[first] = second
+
+
+
+// console.log(personalMovieDB);
+
+
+
+
+// ...............................
+
+
+
+
+
+// let numberOfFilms = +prompt("Qancha film ko'rgansiz?");
+
+// let personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     test: 5,
+//     genres: [],
+//     privat: false
+// };
+
+
+
+// showName('Akmal')
+// showName('Salim')
+// showName('Botir')
+
+// function showName() {
+//     if ()
+//         console.log
+// }
+
+
+
+
+// let numberOfFilms = +prompt("Qancha film ko'rgansiz?");
+
+// let personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// }
+
+// for (let i = 0; i < 3; i++) {
+
+//     let firstQ = prompt("Oxirgi ko'rgan filmlaringizdan biri?");
+//     let secondQ = prompt("Uni qancha baholagan bo'lardingiz?");
+
+//     if (firstQ == '' || firstQ == null || secondQ == '' || secondQ == null) {
+//         i--;
+//     } else {
+//         personalMovieDB.movies[firstQ] = secondQ
+//     }
+// }
+
+// if (personalMovieDB.count <= 10) {
+//     console.log("Ko'rilgan filmlar soni kam")
+// } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+//     console.log("Siz klassik tomoshabinsiz")
+// } else if (personalMovieDB.count > 30) {
+//     console.log("Siz kino ishqibozisiz")
+// } else {
+//     console.log("Xatolik yuz berdi")
+// }
+
+
+// console.log(personalMovieDB)
+
+
+
+
+// let num = prompt("plastik raqamni kiriting"); */
+
+
+/* // alert($ { num.substring(0, 4) } ** ** ** ** $ { num.substring(12, 16) }); */
+
+
+
+
+// function cutName(name1, name2) {
+//     console.log(name1.substring(0, 3) + name2.substring(-3, 0));
+// }
+
+// cutName('Siroj', 'Jahongir');
+
+// let shop = ['apple', 'orange', 'banana', 'kiwi', 'pear', 'melon'];
+
+// for (let i = 1; i < shop.length; i++) {
+//     console.log(i + '-' + "ro'yxat " + shop[i]);
+// }
+
+
+
+// let user = {
+//     name: 'Siroj',
+//     surname: 'Rustamov',
+//     age: 16,
+// }
+
+// console.log('Mening ismim: ' + user.name + ', Familiyam: ' + user.surname + ', Yoshim esa: ' + user.age)
+
+// let result = prompt("ismingiz nima?");
+
+// if (result == "siroj") {
+//     console.log(`Siz ismingizni to'g'ri kiritdingiz`)
+// } else {
+//     console.log(`Sizning ismingiz ${result} emas`)
+// }
+
+// let num = +prompt('Raqam kiriting');
+
+// if (num < 50) {
+//     console.log(`siz 50dan kichik son kiritdingiz`)
+// } else if (+num == 50) {
+//     console.log('Bu son 50ga teng')
+// } else {
+//     console.log(`bu son 50dan katta `)
+// }
+
+
+
+// let choosename = prompt('ismingizni  kiriting ');
+// let siroj = {
+//     name: 'siroj',
+//     surname: 'rustamov',
+//     age: 16,
+// };
+// if (choosename == 'siroj') {
+//     console.log('Ismi: ' + siroj.name + ', Familiyasi esa: ' + siroj.surname + ', Yoshi: ' + siroj.age);
+// } else(choosename == ){
+
+// }
+// let aziz = {
+//     name: 'aziz',
+//     surname: 'ne`matov',
+//     age: 16,
+// }
+// if (choosename == 'aziz') {
+//     console.log('Ismi: ' + aziz.name + ', Familiyasi esa: ' + aziz.surname + ', Yoshi: ' + aziz.age);
+// }
+// let islombek = {
+//     name: 'islombek',
+//     surname: 'mustofaqulov',
+//     age: 16,
+// }
+// if (choosename == 'islombek') {
+//     console.log('Ismi: ' + islombek.name + ', Familiyasi esa: ' + islombek.surname + ', Yoshi: ' + islombek.age);
+// } else {
+//     console.log('bunday ism mavjud emas');
+// }
+
+
+
+
+//homework
+
+// let userName = prompt('Ism kiriting')
+// let ali = {
+//     name: 'Ali',
+//     surname: "Ahmadov",
+//     age: 34
+// }
+// let aziz = {
+//     name: 'Aziz',
+//     surname: "Ashiraliev",
+//     age: 56
+// }
+// let abror = {
+//     name: 'Abror',
+//     surname: "Rustamov",
+//     age: 12
+// }
+// if (userName === "Ali" || userName === "ali") {
+//     console.log(`Ism: ${ali.name}, Familiya: ${ali.surname}`)
+// } 
+
+// let num = 3;
+
+// num -= 2;
+
+// console.log(num)
+
+// for(let i = 0; i < 3; i++){
+//     let p = prompt('Ismingizni kiriting')
+//     if
+// }
+
+// let arr = ['red', 'orange', 'yellow', 'pink', 'blue', ]
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(`$
+
+
+
+//     {i} - inkesda ${arr}`)
+// }
+
+// function calc(a, b) {
+//     if (a || b < 0) {
+//         console.log('Misolda xato mavjud')
+//     } else if (a || b > 0) {
+//         console.log(`P = ${2 * (a + b)}`)
+//         console.log(`S = ${a * b}`)
+//     }
+
+// }
+
+
+// calc(10, 20)
+
+
+// let num = NaN
+// if (num !== NaN) {
+//     console.log('Bu nanga teng')
+// } else(typeof num == typeof Namber) {
+//     console.log(`bu qiymat ${num} ga teng`)
+// }
+
+
+// let numbers = [1, 45, 76, 52, 12, 26, 39]
+
+// let newArr = []
+
+// numbers.forEach(function(item, index, array) {
+//     newArr.push(item * 2)
+// })
+
+// console.log(newArr)
+
+
+
+
+// function snackCaseToCamelCase(str) {
+
+// }
+
+
+// snackCaseToCamelCase('get_time_remaining')
+
+
+
+// let obj = {
+//     name: `Jahongir`,
+//     surname: `Odiljonov`,
+//     age: 22,
+//     some: {
+//         a: 5,
+//         b: {
+//             c: 10
+//         }
+//     }
+// }
+
+// let { name, surname, age, some: { a, b: { c } } } = obj
+// console.log(obj)
+
+
+// let div = document.createElement('div'),
+//     wrapper = document.querySelector('.wrapper')
+
+// div.style.cssText = `
+// width: 100%;
+// height: 300px;
+// background: blue;
+// border-radius: 10px;
+// `
+
+// wrapper.append(div) +
+
+// let block = document.querySelector('.block'),
+//     btn = document.querySelector('button'),
+//     inp = document.querySelector('input')
+
+
+// inp.addEventListener('input', function() {
+//     if (inp, value === 'jahongir') {
+//         alert('done')
+//     }
+// })
+
+
+// let name = "Siroj",
+//     age = 5,
+//     surname = "Rustamov",
+//     school = "41-maktab"
+
+
+// let user = {
+//     name: 'Siroj',
+//     surname: 'Rustamov',
+//     age: 16,
+//     school: "41-maktab"
+// }
+
+// console.log('Mening ismim: ' + user.name + ', Familiyam: ' + user.surname + ', Yoshim: ' + user.age + 'da, ' + user.school + "da o'qiyman")
+
+
+
+// for (let i = 0; i < 3; i++) {
+//     let p = prompt('Ismingizni kiriting')
+//     if
+// }
+
+
+// let firstNumber = prompt('Birinchi sonni kiriting');
+// let seconNumber = prompt('Ikkinchi sonni kiriting');
+
+// console.log(+firstNumber + seconNumber)
+
+let ism = prompt("Ismingizni kiriting");
+let surname = prompt("Familiyangizni kiriting");
+let age = prompt("Yoshingiz nechchida");
+let job = prompt("Kasbingiz nima");
+
+console.log('Ismi: ' + ism + ", Familiyasi: " + surname + ", Yoshi: " + age + ", Kasbi: " + job)
